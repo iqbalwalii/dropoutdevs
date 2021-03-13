ef17c43e62b76b3699b789048e30b88206ca1f08
iqbalwalii <iqbalwali786@gmail.com>
2021-03-13 22:47:58 +0530

    added styles

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.navbar__menu');
+
+hamburger.addEventListener('click', () => {
+menu.classList.toggle('hide')
+})
