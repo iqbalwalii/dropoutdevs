33e8292cf52f039e8ee0acfcd9b685d4d2088495
iqbalwalii <iqbalwali786@gmail.com>
2021-03-16 23:16:53 +0530

    added styles to homepage

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,13 @@
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.navbar__menu');
+const topBtn = document.querySelector('.top');
+console.log(topBtn)
 
 hamburger.addEventListener('click', () => {
 menu.classList.toggle('hide')
+})
+// scroll to top btn
+topBtn.addEventListener('click', () => {
+    window.scrollTo(0,0)
+    
 })
