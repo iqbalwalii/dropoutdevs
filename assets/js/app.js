31e30f66fe2895a2dbd683534cec5f5abc9a636c
iqbalwalii <iqbalwali786@gmail.com>
2021-03-17 15:42:54 +0530

    added responsive size to about and contact pages

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,18 @@
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.navbar__menu');
 const topBtn = document.querySelector('.top');
+const close = document.querySelector('.close');
+const nav= document.querySelector('.navbar__menu')
 console.log(topBtn)
-
 hamburger.addEventListener('click', () => {
-menu.classList.toggle('hide')
+menu.classList.toggle('hide');
 })
+
 // scroll to top btn
 topBtn.addEventListener('click', () => {
     window.scrollTo(0,0)
     
+})
+close.addEventListener('click',()=>{
+    nav.classList.toggle('hide');
 })
