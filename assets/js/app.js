a75c18064bd7a1472c1f50328e1b8f21636928b0
iqbalwalii <iqbalwali786@gmail.com>
2021-03-18 17:42:54 +0530

    finalising changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,8 +3,6 @@ const menu = document.querySelector('.navbar__menu');
 const topBtn = document.querySelector('.top');
 const close = document.querySelector('.close');
 const nav= document.querySelector('.navbar__menu')
-const card = document.querySelector('.')
-console.log(topBtn)
 hamburger.addEventListener('click', () => {
 menu.classList.toggle('hide');
 })
